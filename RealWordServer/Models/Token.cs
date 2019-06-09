80e37f632546a6b82d6af391d0f902f54c3cf693
KK <krishnak.wlc@hotmail.com>
2019-06-08 19:22:24 -0700

    Beginnings of server

---
+++ RealWordServer/Models/Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RealWordServer.Models
+{
+    public class Token
+    {
+        public int TokenId { get; set; }
+        public string Secret { get; set; }
+        public int UserId { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
---
+++ RealWordServer/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace RealWordServer.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
